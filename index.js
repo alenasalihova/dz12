555cf36241200f710519c97aed5449034706fb0c
Salihova Alena <alexx9202@gmail.com>
2023-09-27 21:08:27 +0300

    using static

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
-const http = require('http');
-const {readFileSync} = require('fs');
+const express = require("express");
+const path = require("path");
 
-const htmlHome = readFileSync('./page.html');
-const htmlAbout = readFileSync('./about.html');
-const img = readFileSync('./306165878.jpg');
-const style = readFileSync('./style.css');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    
-    get('/', () => {
-        headers(200, 'text/html');
-        res.end(htmlHome);
-    });
-    get('/about', () => {
-        headers(200, 'text/html');
-        res.end(htmlAbout);
-    });
-    get('/306165878.jpg', () => {
-        headers(200, 'image/jpeg');
-        res.end(img);
-    });
-    get('/style.css', () => {
-        headers(200, 'text/css');
-        res.end(style);
-    });
+app.use(express.static("public"));
 
-    function headers (code, type) {
-        res.writeHead(code, { 'content-type': type});
-    }
-    function get(url, callback) {
-        if (req.url === url) {
-          callback();
-        }
-    }
+app.all('*', (req, res) => {
+    res.status(404).send("<h2>Page not found </h2>");
 });
-
-server.listen(8000);
-
-
-  /*  if (req.url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write(htmlHome);
-        res.end();
-    } else if (req.url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end(htmlAbout);
-    } else if (req.url === '/306165878.jpg') {
-        res.writeHead(200, {'content-type': 'image/jpeg'});
-        res.end(img);
-    } else if (req.url === '/style.css') {
-        res.writeHead(200, {'content-type': 'text/css'});
-        res.end(style);
-    } else {
-        res.writeHead(404, {'content-type': 'text/html'});
-        res.end('page not found');
-    } */
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000');
+  });
+  
