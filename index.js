c50f9a01cbfd3db75f96f24bc113996918e4116e
Salihova Alena <alexx9202@gmail.com>
2023-09-27 16:59:42 +0300

    упрощенное

--- index.js
+++ index.js
@@ -7,7 +7,38 @@ const img = readFileSync('./306165878.jpg');
 const style = readFileSync('./style.css');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    
+    get('/', () => {
+        headers(200, 'text/html');
+        res.end(htmlHome);
+    });
+    get('/about', () => {
+        headers(200, 'text/html');
+        res.end(htmlAbout);
+    });
+    get('/306165878.jpg', () => {
+        headers(200, 'image/jpeg');
+        res.end(img);
+    });
+    get('/style.css', () => {
+        headers(200, 'text/css');
+        res.end(style);
+    });
+
+    function headers (code, type) {
+        res.writeHead(code, { 'content-type': type});
+    }
+    function get(url, callback) {
+        if (req.url === url) {
+          callback();
+        }
+    }
+});
+
+server.listen(8000);
+
+
+  /*  if (req.url === '/') {
         res.writeHead(200, {'content-type': 'text/html'});
         res.write(htmlHome);
         res.end();
@@ -21,9 +52,6 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'content-type': 'text/css'});
         res.end(style);
     } else {
-        res.writeHead(200, {'content-type': 'text/html'});
+        res.writeHead(404, {'content-type': 'text/html'});
         res.end('page not found');
-    }
-});
-
-server.listen(8000);
+    } */
