544ba64675cf3354e411c0edd28cfee501a89ea9
Salihova Alena <alexx9202@gmail.com>
2023-09-28 17:03:03 +0300

    Routing

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
-
+//app.use(express.static("public"));
+app.get ('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/index.html'));
+});
+app.get ('/about.html', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/about.html'));
+});
+app.get ('/style.css', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/style.css'));
+});
+app.get ('/style1.css', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/style1.css'));
+});
+app.get ('/306165878.jpg', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/306165878.jpg'));
+});
+app.get ('/kandinsky.jpeg', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/kandinsky.jpeg'));
+});
 app.all('*', (req, res) => {
     res.status(404).send("<h2>Page not found </h2>");
 });
@@ -12,3 +29,5 @@ app.listen(8000, () => {
     console.log('Example app listening on port 8000');
   });
   
+
+ 
